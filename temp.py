fcc351be859ebec134e2ad59fc990c4cf6f64e5d
mehtadinesh1 <nishant.111.mehta@gmail.com>
2024-03-26 11:20:03 +0530

    Instructions is added

--- temp.py
+++ temp.py
@@ -4,5 +4,6 @@ user = input('Are you ready = ')
 if user == 'ready':
     print('Give answer sheet')
     print('Give question paper')
+    print('Give instruction')
 else:
     print('There is still time left for exam')
