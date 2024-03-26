b6f1a7f909a3372e5c32c750aff24413b02a9aea
mehtadinesh1 <nishant.111.mehta@gmail.com>
2024-03-26 11:15:10 +0530

    Question paper added

--- temp.py
+++ temp.py
@@ -1,1 +1,8 @@
 print('This is git test')
+user = input('Are you ready = ')
+
+if user == 'ready':
+    print('Give answer sheet')
+    print('Give question paper')
+else:
+    print('There is still time left for exam')
